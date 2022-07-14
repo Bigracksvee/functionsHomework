c1eac50e8ce956ae032412a9f55129e1b09ac3f7
Vera <veraopoku100@gmail.com>
2022-07-14 07:00:33 -0400

    pushing with functionHomework

---
+++ apps.js
@@ -0,0 +1,25 @@
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+var g = 7;
+var h = 8;
+var i = 9;
+var j = 10;
+
+console.log(a+b);
+console.log(a-b);
+console.log(c+d);
+console.log(c-d);
+console.log(e*f);
+console.log(e/f);
+console.log(g*h);
+console.log(g/h);
+
+console.log(a++);
+console.log(b++);
+console.log(c--);
+console.log(d--);
+
